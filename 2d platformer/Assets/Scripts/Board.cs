bb79785a96ac727afdcdf10898b10b3e77e2d53c
ppv-1 <virus9320@gmail.com>
2024-08-26 13:01:19 +0800

    7 bag system using Queue

--- 2d platformer/Assets/Scripts/Board.cs
+++ 2d platformer/Assets/Scripts/Board.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using System;
 
 public class Board : MonoBehaviour
 {
     public TetrominoData[] tetrominos;
     public Piece activePiece { get; private set; }
+    public int pieceCount;
+    public TetrominoData[] bag;
+    public Queue<TetrominoData> pieceQueue = new Queue<TetrominoData>();
     public Tilemap tilemap { get; private set; }
     public Vector3Int spawnPosition;
     public Vector2Int boardSize = new Vector2Int(10, 22);
@@ -23,18 +28,61 @@ public class Board : MonoBehaviour
         for(int i = 0; i < this.tetrominos.Length; i++){
             this.tetrominos[i].Initialize();
         }
+        // for bag use
+        for(int i = 0; i < this.bag.Length; i++){
+            this.bag[i].Initialize();
+        }
+        // first bag
+        RandomizeBag(bag);
+        foreach(TetrominoData t in bag){
+            this.pieceQueue.Enqueue(t);
+        }
+        
+
     }
 
     private void Start(){
         SpawnPiece();
     }
 
+    private void Update(){
+        if(this.pieceCount % 7 == 0){
+            ManageQueue();
+        }
+    }
+
+    static void RandomizeBag<T>(T[] array)
+    {
+        System.Random rng = new System.Random();
+        int n = array.Length;
+
+        // Fisher-Yates shuffle algorithm
+        for (int i = n - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);  // Random index from 0 to i
+            // Swap array[i] with array[j]
+            T temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+
+    public void ManageQueue(){
+        RandomizeBag(bag);
+        foreach(TetrominoData t in bag){
+            this.pieceQueue.Enqueue(t);
+        }
+        
+    }
+
     public void SpawnPiece(){
-        int random = Random.Range(0, this.tetrominos.Length);
-        TetrominoData data = this.tetrominos[random];
+        // int random = Random.Range(0, this.tetrominos.Length;
+        
+        TetrominoData data = this.pieceQueue.Dequeue();
         this.activePiece.Initialize(this, spawnPosition, data);
         if(IsValidPosition(this.activePiece, this.spawnPosition)){
             Set(activePiece);
+            this.pieceCount++;
         } else{
             GameOver();
         }
